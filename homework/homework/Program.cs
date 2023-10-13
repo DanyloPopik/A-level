4badb9108d21f6241953fec1a218bf78f3d5c5cf
Danylo <danyapopik715@gmail.com>
2023-10-13 21:21:13 +0300

    First commit

---
+++ homework/homework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = new int[] { 5, 10, 25,78,};
+        int count = 0;
+
+        foreach (int value in array)
+        {
+            if (value >= -100 && value <= 100)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("The number of elements in the range -100 to +100: " + count);
+    }
+}
---
+++ homework1/homework1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N = 20;
+        int[] A = new int[N];
+        int[] B = new int[N];
+
+        
+        int Min = -1000; 
+        int Max = 1000;  
+        Random random = new Random();
+
+        for (int i = 0; i < N; i++)
+        {
+            A[i] = random.Next(Min, Max + 1);
+        }
+
+       
+        int BIndex = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            if (A[i] <= 888)
+            {
+                B[BIndex] = A[i];
+                BIndex++;
+            }
+        }
+
+        
+        Array.Sort(B, 0, BIndex);
+        Array.Reverse(B, 0, BIndex);
+
+        
+        Console.WriteLine("Array B (sorted in descending order and <= 888):");
+        for (int i = 0; i < BIndex; i++)
+        {
+            Console.Write(B[i] + " ");
+        }
+    }
+}
